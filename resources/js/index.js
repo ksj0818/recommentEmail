30212a97e6d418f123f09b69569dd6140ad5f51e
ksj0818 <ksj071808@gmail.com>
2021-09-24 16:54:06 +0900

    Update index.js
    이메일 자동완성 복습

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,52 +1,56 @@
 /*
 1. Element 가져온다.
 2. Element 이벤트를 건다.
+
+includes() 메서드는 배열이 특정 요소를 포함하고 있는지 판별 / true, false로 반환
+indexOf() 메서드는 배열에서 지정된 요소를 찾을 수 있는 첫 번째 인덱스를 반환하고 존재하지 않으면 -1을 반환
+filter() 메서드는 주어진 함수의 테스트를 통과하는 모든 요소를 모아 새로운 배열로 반환
 */
-const emails = [
-  'gmail.com',
+
+const inputId = document.getElementById('inputId');
+const emailGroup = document.getElementById('emailGroup');
+const emailList = [
+  'google.com',
   'naver.com',
-  'kakao.com',
-  'daum.net',
   'nate.com',
-  'hanmail.com',
-  'example.com',
-  'dreamwiz.com',
-  'yahoo.com'
-]
-let filterEmails = []
+  'daum.net',
+  'yahoo.com',
+  'hanmail.net',
+  'ffasdas.com',
+];
+let filterEmailList = []
 
-const listElement = document.getElementById('listElement')
-listElement.style.display = 'none'
+inputId.addEventListener('input', recommentEmail)
 
-const inputElement = document.getElementById('inputElement')
-inputElement.addEventListener('input', function(event) {
+function recommentEmail(event) {
+  let autoList = '';
   if (event.target.value.includes('@')) {
-    listElement.style.display = 'block'
-    const id = event.target.value.substr(0, event.target.value.indexOf('@') + 1)
-    const keyword = event.target.value.substr(event.target.value.indexOf('@') + 1, event.target.value.length)
-    console.log(keyword);
+    let id = event.target.value.substr(0, event.target.value.indexOf('@') + 1)
+    let keyword = event.target.value.substr(event.target.value.indexOf('@') + 1, event.target.value.length)
 
-    filterEmails = emails.filter((email) => {
+    filterEmailList = emailList.filter(email => {
       if (email.includes(keyword)) {
         return email
       }
     })
 
-    let html = ''
-    for (let email of filterEmails) {
-      html += `<li class="list-group-item" onclick="setEmail('${id}${email}')">${id}<mark>${email}</mark></li>`
-    }    
-    listElement.innerHTML = html
+    for (let i = 0; i < filterEmailList.length; i++) {
+      let idMail = `${id}${filterEmailList[i]}`
+      autoList += `<li class="list-group-item" onclick="setEmail('${idMail}')">${idMail}</li>`
+    }
+    emailGroup.innerHTML = autoList;
+    emailGroup.style.display = "block";
   } else {
-    listElement.style.display = 'none'
+    emailGroup.style.display = "none";
   }
-})
+}
 
-inputElement.addEventListener('focus', function() {
-  listElement.style.display = 'block'
+inputId.addEventListener('focus', function(event) {
+  recommentEmail(event)
+  emailGroup.style.display = "block";
 })
 
 function setEmail(id) {
-  inputElement.value = id;
-  listElement.style.display = 'none'
+  inputId.value = id
+  emailGroup.style.display = "none";
 }
